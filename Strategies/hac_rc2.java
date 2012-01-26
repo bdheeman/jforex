061569875d8c2ca700d17de951c57234271279c0
Balwinder S Dheeman <bdheeman@gmail.com>
2012-01-27 02:50:06 +0530

    added copyright and short license notice

--- Strategies/hac_rc2.java
+++ Strategies/hac_rc2.java
@@ -1,3 +1,20 @@
+//
+// Copyright (c) 2012, Balwinder S "bdheeman" Dheeman <bdheeman/AT/gmail.com>
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc., 675
+// Mass Ave, Cambridge, MA 02139, USA.
+//
 package jforex.strategies.bdheeman;
 
 import java.util.HashSet;
@@ -133,7 +150,7 @@ public class hac_rc2 implements IStrategy {
         final int LOW = 2;
         final int CLOSE = 1;
 
-        double average = (ha[PREV][HIGH] - ha[PREV][LOW] + ha[CURR][HIGH] - ha[CURR][LOW] + ha[THIS][HIGH] - ha[THIS][LOW]) / 3;
+        double average = ((ha[PREV][HIGH] - ha[PREV][LOW]) + (ha[CURR][HIGH] - ha[CURR][LOW]) + (ha[THIS][HIGH] - ha[THIS][LOW])) / 3;
         if (average < threshold)
             return;
 
